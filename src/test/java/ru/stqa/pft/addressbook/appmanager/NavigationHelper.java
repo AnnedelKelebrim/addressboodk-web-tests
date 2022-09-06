34f5d6269141ffae3d3b112d999d44fbb332afda
AnnedelKelebrim <annedel@mail.ru>
2022-09-06 19:26:37 +0600

    Добавила проверку для перехода на страницу с созданием контакта

--- src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -26,6 +26,11 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void goToCreateContactPage() {
+        if(isElementPresent(By.tagName("h1"))
+        && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")
+        && isElementPresent(By.name("submit"))){
+            return;
+        }
         click(By.linkText("add new"));
     }
 }
