36af95c7873d0a6059b8c38bc2a867b6ee0bcb05
AnnedelKelebrim <annedel@mail.ru>
2022-10-11 23:38:52 +0600

    Докрутила проверку списков при удалении контакта

--- src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -18,10 +18,15 @@ public class ContactDeletionTests extends TestBase {
                     (new ContactData("Самуил", "Яковлевич", "Для удаления", "Самоха", "Писатель", "Союз писателей", "г.Чёртовы Кулички, д.4", "488-09-94", "79280398811", "kulichki@mail.ru", "16", "November", "1800", "Test1", "г. Чёртовы Кулички, д.15, кв.1", "777-66-55", "Давайте всё получится?"));
         }
         List<ContactData> before = app.getContactHelper().getContactList();
-        app.getContactHelper().selectContact(before.get(before.size() - 1).getId());
+        int contactId = before.get(before.size() - 1).getId();
+        app.getContactHelper().selectContact(contactId);
         app.getContactHelper().deleteContact();
         app.getNavigationHelper().goToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
+
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(after, before);
     }
 }
