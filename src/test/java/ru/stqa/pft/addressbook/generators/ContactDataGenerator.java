14097ee25ed697c110bf122665c6ae480f38dee6
AnnedelKelebrim <annedel@mail.ru>
2022-10-18 21:09:27 +0600

    Генераторы данных для групп и контактов

---
+++ src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook.generators;
+
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+    public static void main(String[] args) throws IOException {
+        int count = Integer.parseInt(args[0]);
+        File file = new File(args[1]);
+        List<ContactData> contacts = generateContacts(count);
+        save(contacts, file);
+    }
+
+    private static void save(List<ContactData> contacts, File file) throws IOException {
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s\n", contact.getFirstName(), contact.getLastName(), contact.getHomePhone()));
+        }
+        writer.close();
+    }
+
+    private static List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new ContactData().withFirstName(String.format("Иннокентий %s", i))
+                    .withLastName(String.format("Петровский %s", i))
+                    .withHomePhone(String.format("85-85-5%s", i)));
+        }
+        return contacts;
+    }
+}
