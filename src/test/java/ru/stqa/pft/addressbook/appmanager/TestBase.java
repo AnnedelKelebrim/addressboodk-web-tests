0d58717610478328558e2ef8945907089ffd2aab
AnnedelKelebrim <annedel@mail.ru>
2022-10-21 18:09:51 +0600

    Нерабочий запуск из консоли

--- src/test/java/ru/stqa/pft/addressbook/appmanager/TestBase.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(Browser.CHROME.toString());
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
