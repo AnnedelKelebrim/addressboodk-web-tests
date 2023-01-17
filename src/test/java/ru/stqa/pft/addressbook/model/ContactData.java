21d0d560381d44bba4026226b7ff5718a57ca23e
AnnedelKelebrim <annedel@mail.ru>
2023-01-17 22:35:20 +0600

    Заработало почти

--- src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -32,33 +32,40 @@ public class ContactData {
     private String company;
     @Expose
     @Column(name = "address")
+    @Type(type = "text")
     private String firstAddress;
     @Expose
     @Column(name = "home")
+    @Type(type = "text")
     private String homePhone;
     @Expose
     @Column(name = "mobile")
+    @Type(type = "text")
     private String mobilePhone;
     @Expose
     @Column(name = "work")
+    @Type(type = "text")
     private String workPhone;
     @Expose
     @Transient
     private String allPhones;
     @Expose
     @Column(name = "email")
+    @Type(type = "text")
     private String firstEmail;
     @Expose
     @Column(name = "email2")
+    @Type(type = "text")
     private String secondEmail;
     @Expose
     @Column(name = "email3")
+    @Type(type = "text")
     private String thirdEmail;
     @Expose
     @Transient
     private String allEmails;
     @Expose
-    @Type(type = "tinyint")
+    @Type(type = "byte")
     private String bday;
     @Expose
     private String bmonth;
@@ -69,10 +76,13 @@ public class ContactData {
     private String group;
     @Expose
     @Column(name = "address2")
+    @Type(type = "text")
     private String secondAddress;
     @Expose
+    @Type(type = "text")
     private String notes;
     @Expose
+    @Type(type = "text")
     private String photo;
 
 
@@ -249,7 +259,7 @@ public class ContactData {
     }
 
     public ContactData withBday(String bday) {
-        this.bday = bday;
+        this.bday = bday.;
         return this;
     }
 
